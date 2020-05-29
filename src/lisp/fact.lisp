922518664f9ba363dc9496895f7af2c8cf41c8cd
h908714124 <kraftdurchblumen@gmx.de>
2020-05-29 21:45:52 +0200

    cleanup

--- src/lisp/fact.lisp
+++ src/lisp/fact.lisp
@@ -1,11 +1,11 @@
-(def true ((lambda (a b) a)))
-(def false ((lambda (a b) b)))
+(def true (lambda (a b) a))
+(def false (lambda (a b) b))
 
 (def zero? (lambda (n) (n (lambda (x) false) true)))
 
 (def * (lambda (m n) (lambda (f x) (m (n f) x))))
 
-(def pred (lambda (n) (lambda (f x) ((n (lambda (g h) (h (g f))) (lambda (u) x)) (lambda (u1) u1)))))
+(def pred (lambda (n) (lambda (f x) ((n (lambda (g h) (h (g f))) (lambda (u) x)) (lambda (u) u)))))
 
 (def 1 (lambda (f x) (f x)))
 (def 2 (lambda (f x) (f (f x))))
