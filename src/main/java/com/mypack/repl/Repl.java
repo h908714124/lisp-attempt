0dfe2b3a218622f38e8e96562c50c053e3d44593
h908714124 <kraftdurchblumen@gmx.de>
2020-06-22 13:45:59 +0200

    use jline

--- src/main/java/com/mypack/repl/Repl.java
+++ src/main/java/com/mypack/repl/Repl.java
@@ -2,26 +2,26 @@ package com.mypack.repl;
 
 import com.mypack.eval.Environment;
 import com.mypack.exp.Exp;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class Repl {
 
     public static void main(String[] args) throws IOException {
         Environment env = new Environment();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String line;
-            System.out.print("$ ");
-            System.out.flush();
-            while (!Objects.toString(line = reader.readLine(), "").trim().isEmpty()) {
-                Exp exp = env.eval(line);
-                System.out.println(exp.toString());
-                System.out.print("$ ");
-                System.out.flush();
-            }
+        Terminal terminal = TerminalBuilder.terminal();
+        LineReader lineReader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+        String line;
+        while (!Objects.toString(line = lineReader.readLine("$ "), "").trim().isEmpty()) {
+            Exp exp = env.eval(line);
+            terminal.writer().println(exp.toString());
         }
     }
 }
